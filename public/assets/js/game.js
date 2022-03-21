86881a86ac4cbb10488a901d0be833a5935001e6
Malin Olsson <miragia.solinde@gmail.com>
2022-03-21 14:44:08 +0100

    Changed code in game.js
    Added and removed code from game.js to get it to work

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -112,62 +112,68 @@ messageForm.addEventListener('submit', e => {
 
 /**
  * 
- * Heidi
+ * Heidi och Malin
  */
 
-"use strict";  
-  //const socket = io();
-  const container = document.querySelector(".container");
-  const users = document.querySelector(".users");
-  // const usersScore = document.querySelector(".usersCore")
+ "use strict";  
+ //const socket = io();
+ const container = document.querySelector(".container");
+ const users = document.querySelector(".users");
+ // const usersScore = document.querySelector(".usersCore")
 
-  const virus = document.createElement("img");
-  virus.setAttribute("id", "virus-icon");
-  virus.setAttribute('style', `grid-column-start: ${randomColumnRow()}; grid-row-start: ${randomColumnRow()}`)
-  virus.setAttribute("src", "assets/icons/corona-virus.svg");
-  container.appendChild(virus);
+ const virus = document.createElement("img");
+ virus.setAttribute("id", "virus-icon");
+ virus.setAttribute('style', `grid-column-start: ${randomColumnRow()}; grid-row-start: ${randomColumnRow()}`)
+ virus.setAttribute("src", "assets/icons/corona-virus.svg");
+ container.appendChild(virus);
 
-  
+ function randomColumnRow () {
+   return Math.ceil(Math.random() * 8)
+ }
+ 
+ const changeVirusPosition = () => {
+	 const row = randomColumnRow();
+	 const column = randomColumnRow();
+   
+	 virus.style.gridColumnStart = column;
+	 virus.style.gridRowStart = row;
+   }
 
-  let score = 0;
-
-
-function randomColumnRow () {
-  return Math.ceil(Math.random() * 8)
-}
-
-const changeVirusPosition = () => {
-    const row = randomColumnRow();
-    const column = randomColumnRow();
-  
-    virus.style.gridColumnStart = column;
-    virus.style.gridRowStart = row;
-  }
-
-    // Testfunction för att kolla rörelsen
-  // setInterval(changeVirusPosition, 2000);
-
-
-    // Eventlistner listening after a click on the virus and adding one to the score
-    container.addEventListener("click",(e) =>{
-      if (e.target === virus) {
-        score++;
-
-        let min = 2;
-        let max = 5;
-
-        //Generate Random number between 5 - 10,
-        //SetTimeout för att det ska vara väntan mellan kilcksen 
-        let rand = Math.floor(Math.random() * (max - min + 1) + min); 
-        console.log('score ', score)
-        setTimeout(() => { 
-        
-        document.getElementById("users").innerHTML="Your score is: " + score + "point";
-        changeVirusPosition()
-
-      },rand * 1000);
-      }
-    
-    });
-    
+ 
+ 
+ let clickedTime, createdTime, reactionTime;
+ 
+ makeVirus = () => {
+   let time = Math.random();
+   time=time*5000;
+   
+   setTimeout(function() {
+	 if (Math.random() > 0.5) {
+	   document.getElementById("virus-icon").style.borderRadius="100px";
+	 } else {
+	   document.getElementById("virus-icon").style.borderRadius="0";
+	 }
+	 
+	 let top=Math.random();
+	 top=top*300;
+	 
+	 let left=Math.random();
+	 left=left*500;
+	 
+	 changeVirusPosition();
+	 
+	 document.getElementById("virus-icon").style.display = "block";
+	 createdTime = Date.now();
+   }, time);
+ }
+ 
+ document.getElementById("virus-icon").onclick = function() {
+   clickedTime = Date.now();
+   reactionTime = (clickedTime-createdTime)/1000;
+   document.getElementById("time").innerHTML = reactionTime;
+   this.style.display = "none";
+   makeVirus();
+ }
+ 
+ makeVirus();
 
