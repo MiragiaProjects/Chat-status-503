d400577e38cb34663ddbc6fef7e9beb37172f996
Heidi sjöberg <heidi.alexx8@gmail.com>
2022-03-24 13:33:54 +0100

    first commit

---
+++ controllers/socket_controller.js
@@ -0,0 +1,192 @@
+/***
+ * 
+ * Socket Controller
+ */
+
+ const debug = require('debug')('game:socket_controller');
+ let io = null; // socket.io server instance
+
+// list of rooms and their connected users
+const users = {}
+const rooms = [
+	{
+		id: 'room1',
+		name: 'Room one',
+		users: {},
+	},
+	{
+		id: 'room2',
+		name: 'Room two',
+		users: {},
+	},
+	{
+		id: 'room3',
+		name: 'Room three',
+		users: {},
+	},
+	{
+		id: 'room4',
+		name: 'Room four',
+		users: {},
+	},
+];
+
+const handleDisconnect = function() {
+	debug(`Client ${this.id} disconnected :(`);
+
+	// find the room that this socket is part of
+	const room = rooms.find(chatroom => chatroom.users.hasOwnProperty(this.id));
+
+	// if socket was not in a room, don't broadcast disconnect
+	if (!room) {
+		return;
+	}
+
+	// let everyone in the room know that this user has disconnected
+	this.broadcast.to(room.id).emit('user:disconnected', room.users[this.id]);
+
+	// remove user from list of users in that room
+	delete room.users[this.id];
+
+	// broadcast list of users in room to all connected sockets EXCEPT ourselves
+	this.broadcast.to(room.id).emit('user:list', room.users);
+}
+
+// Handle when a user has joined the chat
+const handleUserJoined = function(username, room_id, callback) {
+	debug(`User ${username} with socket id ${this.id} wants to join room '${room_id}'`);
+
+	// join room
+	this.join(room_id);
+
+	// add socket to list of online users in this room
+	// a) find room object with `id` === `general`
+	const room = rooms.find(chatroom => chatroom.id === room_id)
+
+	// b) add socket to room's `users` object
+	room.users[this.id] = username;
+
+	// let everyone know that someone has connected to the chat
+	this.broadcast.to(room.id).emit('user:connected', username);
+
+	// confirm join
+	callback({
+		success: true,
+		roomName: room.name,
+		users: room.users
+	});
+
+	// broadcast list of users in room to all connected sockets EXCEPT ourselves
+	this.broadcast.to(room.id).emit('user:list', room.users);
+}
+
+
+
+
+
+
+// Handle when a user has joined the chat
+const handleUserFire = function(username, room_id, time) {
+	
+
+
+	// debug(`User ${username} with socket id ${this.id} wants to join room '${room_id}'`);
+	function randomColumnRow () {
+		return Math.ceil(Math.random() * 8)
+	}
+ 
+	const row = randomColumnRow();
+	const column = randomColumnRow();
+
+	const room = rooms.find(rom => rom.id === room_id)
+
+	if (!room.points) {
+        room.points = []
+    }
+    const point = { 
+		username: username,point: time
+    }
+	
+    room.points.push(point)
+    console.log({rooms});
+
+	room.points.forEach(element => {
+		console.log(element);
+		if (username === element.username) {
+			element.point
+			console.log(room.points);
+		}
+
+		room.users[this.id] = username;
+
+
+
+// broadcast list of users in room to all connected sockets EXCEPT ourselves
+
+        this.broadcast.to(room.id).emit('room:point', username);
+
+        console.log(point)
+	});
+	
+
+	
+		//  { --> Room
+		// 	id: 'room1',
+		// 	name: 'Room one',
+		// 	users: {
+		// 		gsg2NtXO0QOuDnPwAAAF: 'Heidi',
+		// 		yZk0ubEbX_hP72jmAAAH: 'daniel'
+		// 	}
+		// }
+
+  
+	console.log({room, username, time})
+	io.to(room.id).emit('damageDone', username, time, row, column);
+
+		
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const handleChatMessage = function(message) {
+	debug('Someone said something: ', message);
+
+	// emit `chat:message` event to everyone EXCEPT the sender
+	this.broadcast.to(message.room).emit('chat:message', message);
+}
+
+module.exports = function(socket, _io) {
+	io = _io;
+
+	debug('a new client has connected', socket.id);
+
+
+
+	io.emit("new-connection", "A new user connected");
+
+	io.emit("new-connection", "A new user connected");
+
+	// io.to(room).emit();
+	socket.on('user:fire', handleUserFire)
+
+	// handle user disconnect
+	socket.on('disconnect', handleDisconnect);
+
+	// handle user joined
+	socket.on('user:joined', handleUserJoined);
+
+	// handle user emitting a new message
+	socket.on('chat:message', handleChatMessage);
+}
---
+++ public/assets/js/game.js
@@ -0,0 +1,231 @@
+const socket = io();
+
+const startEl = document.querySelector('#start');
+const chatWrapperEl = document.querySelector('#game-wrapper');
+const usernameForm = document.querySelector('#username-form');
+const messagesEl = document.querySelector('#messages'); // ul element containing all messages
+const messageForm = document.querySelector('#message-form');
+const messageEl = document.querySelector('#message');
+
+
+let room = null;
+let username = null;
+
+const addMessageToChat = (message, ownMsg = false) => {
+	// create new `li` element
+	const liEl = document.createElement('li');
+
+	// set class of `li` to `message`
+	liEl.classList.add('message');
+
+	if (ownMsg) {
+		liEl.classList.add('you');
+	}
+
+	// get human readable time
+	const time = moment(message.timestamp).format('HH:mm:ss');
+
+	// set content of `li` element
+	liEl.innerHTML = ownMsg
+		? message.content
+		: `<span class="user">${message.username}</span><span class="content">${message.content}</span><span class="time">${time}</span>`;
+
+	// append `li` element to `#messages`
+	messagesEl.appendChild(liEl);
+
+	// scroll `li` element into view
+	liEl.scrollIntoView();
+}
+
+const addNoticeToChat = notice => {
+	const liEl = document.createElement('li');
+	liEl.classList.add('notice');
+
+	liEl.innerText = notice;
+
+	messagesEl.appendChild(liEl);
+	liEl.scrollIntoView();
+}
+
+// update user list
+const updateUserList = users => {
+	document.querySelector('#online-users').innerHTML =
+		Object.values(users).map(username => `<li><span class="fa-solid fa-user-astronaut"></span> ${username}</li>`).join("");
+}
+
+// listen for when a new user connects
+socket.on('user:connected', (username) => {
+	addNoticeToChat(`${username} connected 🥳`);
+});
+
+// listen for when a user disconnects
+socket.on('user:disconnected', (username) => {
+	addNoticeToChat(`${username} disconnected 😢`);
+});
+
+// listen for when we receive an updated list of online users (in this room)
+socket.on('user:list', users => {
+	updateUserList(users);
+})
+
+// listen for when we're disconnected
+socket.on('disconnect', (reason) => {
+	if (reason === 'io server disconnect') {
+		// reconnect to the server
+		socket.connect();
+	}
+	addNoticeToChat(`You were disconnected. Reason: ${reason} 😳`);
+});
+
+// listen for when we're reconnected
+socket.on('reconnect', () => {
+	// join room? but only if we were in the chat previously
+	if (username) {
+		socket.emit('user:joined', username, room, (status) => {
+			addNoticeToChat(`You reconnected 🥳`);
+		});
+	}
+});
+
+socket.on('damageDone', (username, time, row, column) => {
+	addNoticeToChat(`Damage done from ${username} in ${time}`);
+	makeVirus(row, column);
+})
+
+// listen for incoming messages
+socket.on('chat:message', message => {
+	console.log("Someone said something:", message);
+
+	addMessageToChat(message);
+});
+
+// get username and room from form and emit `user:joined` and then show chat
+usernameForm.addEventListener('submit', e => {
+	e.preventDefault();
+
+	room = usernameForm.room.value;
+	username = usernameForm.username.value;
+
+	console.log(`User ${username} wants to join room '${room}'s`);
+
+	// emit `user:joined` event and when we get acknowledgement, THEN show the chat
+	socket.emit('user:joined', username, room, (status) => {
+		// we've received acknowledgement from the server
+		console.log("Server acknowledged that user joined", status);
+
+		if (status.success) {
+			// hide start view
+			startEl.classList.add('hide');
+
+			// show chat view
+			chatWrapperEl.classList.remove('hide');
+
+			// set room name as chat title chat-title
+			document.querySelector('#room').innerText = status.roomName;
+
+			// focus on inputMessage
+			messageEl.focus();
+
+			// update list of users in room
+			updateUserList(status.users);
+		}
+	});
+});
+
+// send message to server
+messageForm.addEventListener('submit', e => {
+	e.preventDefault();
+
+	if (!messageEl.value) {
+		return;
+	}
+
+	const msg = {
+		username,
+		room,
+		content: messageEl.value,
+		timestamp: Date.now(),
+	}
+
+	// send message to server
+	socket.emit('chat:message', msg);
+
+	// add message to chat
+	addMessageToChat(msg, true);
+
+	// clear message input element and focus
+	messageEl.value = '';
+	messageEl.focus();
+});
+
+/**
+ * 
+ * Heidi och Malin
+ */
+
+ "use strict";  
+ //const socket = io();
+ const container = document.querySelector(".container");
+ const users = document.querySelector(".users");
+ // const usersScore = document.querySelector(".usersCore")
+
+ const virus = document.createElement("img");
+ virus.setAttribute("id", "virus-icon");
+ virus.setAttribute('style', `grid-column-start: ${8}; grid-row-start: ${8}`)
+ virus.setAttribute("src", "assets/icons/corona-virus.svg");
+ container.appendChild(virus);
+
+
+
+const changeVirusPosition = (row, column) => {
+	 virus.style.gridColumnStart = column;
+	 virus.style.gridRowStart = row;
+   }
+
+ 
+ 
+ let clickedTime, createdTime, reactionTime;
+ 
+ makeVirus = (row, column) => {
+   let time = Math.random();
+   time=time*5000;
+   
+   setTimeout(function() {
+	 if (Math.random() > 0.5) {
+	   document.getElementById("virus-icon").style.borderRadius="100px";
+	 } else {
+	   document.getElementById("virus-icon").style.borderRadius="0";
+	 }
+	 
+	 let top=Math.random();
+	 top=top*300;
+	 
+	 let left=Math.random();
+	 left=left*500;
+	 
+	 changeVirusPosition(row, column);
+	 
+	 document.getElementById("virus-icon").style.display = "block";
+	 createdTime = Date.now();
+   }, time);
+ }
+
+ document.getElementById("virus-icon").onclick = function() {
+   clickedTime = Date.now();
+   reactionTime = (clickedTime-createdTime)/1000;
+   document.getElementById("time").innerHTML = reactionTime;
+	 this.style.display = "none";
+	 socket.emit('user:fire', username, room, reactionTime);
+ }
+
+
+ 
+ 
+ makeVirus(2,2);
+
+
+
+
+
+
+
